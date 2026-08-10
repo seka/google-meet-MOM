1f481595317b6745320f6390cf65f23b0cbbb8ec
Shin Sekaryo <s.seka134704@gmail.com>
2026-08-10 15:01:42 +0900

    refactor(settings): keep page composition in options entry

--- src/pages/options/options.ts
+++ src/pages/options/options.ts
@@ -1,3 +1,9 @@
-import { initializeSettingsPage } from "@features/theme-settings/components/settings-page/settings-page";
+import { initializeAboutSection } from "@features/theme-settings/components/about-section/about-section";
+import { initializeConnectionTest } from "@features/theme-settings/components/connection-test/connection-test";
+import { initializeSettingsForm } from "@features/theme-settings/components/settings-form/settings-form";
+import { initializeSettingsNavigation } from "@features/theme-settings/components/settings-navigation/settings-navigation";
 
-initializeSettingsPage();
+initializeSettingsNavigation();
+initializeSettingsForm();
+initializeConnectionTest();
+initializeAboutSection();
